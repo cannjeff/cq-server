bf665ae1985f6beaab128a324c2aaeaad9c510e3
Jeff Cann <cannjeff@gmail.com>
2015-08-12 19:00:26 -0400

    added install script

---
+++ install_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Stopping server..."
+forever stop cq-server.js
+
+echo "Removing node_modules..."
+rm -r node_modules
+
+echo "Cleaning npm cache..."
+npm cache clean
+
+echo "Installing dependencies..."
+npm install
+
+echo "Starting server..."
+forever start cq-server.js
+
+echo "Done..."
